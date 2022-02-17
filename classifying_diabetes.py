acb1095408744c9a0192274afcc82adc4a44022f
JiPang22 <wlvud6902@gmail.com>
2022-02-17 09:01:53 +0900

    add diabetes

--- classifying_diabetes.py
+++ classifying_diabetes.py
@@ -15,7 +15,7 @@ b = tf.Variable(tf.random_normal([1]), name='bias')
 
 hypothesis = tf.sigmoid(tf.matmul(X,W) + b)
 cost = -tf.reduce_mean(Y * tf.log(hypothesis)+(1-Y)*tf.log(1-hypothesis))
-train = tf.train.GradientDescentOptimizer(learning_rate=1e-4).minimize(cost)
+train = tf.train.GradientDescentOptimizer(learning_rate=0.5e-4).minimize(cost)
 
 predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
@@ -32,4 +32,4 @@ with tf.Session() as sess:
             print(step, sess.run(cost, feed_dict=feed))
 
     h, c, a = sess.run([hypothesis, predicted, accuracy], feed_dict=feed)
-    print("\nHypothesis: ", "\nCorrect (Y): ", c, "\nAccuracy: ", a)        
+    print("\nHypothesis: ", "\nCorrect (Y): ", c, "\nAccuracy: ", a)
